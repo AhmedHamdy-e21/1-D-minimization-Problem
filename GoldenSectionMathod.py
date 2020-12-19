282791f3ceccd54347dc8fed9d2f9bc53fd60787
Ahmed Hamdy <ahmedhamdy.e21@gmail.com>
2020-12-19 19:40:05 +0200

    Golden Section method is working

--- GoldenSectionMathod.py
+++ GoldenSectionMathod.py
@@ -3,6 +3,7 @@ def swap(xl,xu):
     return xu,xl
 def Fexample(x):
     return 0.65-(0.75/(1+x**2))-0.65*x*atan(1/x)
+    # return 2*sin(x)-((x**2)/10)
 
 def swaplarger(xl,xu,fl,fu):
 
@@ -13,16 +14,17 @@ def swaplarger(xl,xu,fl,fu):
 
 def GoldenSection(A1,B1,N):
     J=1
-    xl=0
-    xu=0
+    xl=A1
+    xu=B1
+    d = 0.618 * (B1 - A1)
+    xl=xl+d
+    xu=xu-d
+    fl = Fexample(xl)
+    fu = Fexample(xu)
+    #
+    xl, xu, fl, fu = swaplarger(xl, xu, fl, fu)
     while J<N:
-        d=0.618*(B1-A1)
-        xl=xl+d
-        xu=xu-d
-
-
-        fl = Fexample(xl)
-        fu = Fexample(xu)
+        print(xl, xu, fl, fu)
         if (fl > fu):
             A1 = xl
             d=0.618*(B1-A1)
@@ -33,10 +35,12 @@ def GoldenSection(A1,B1,N):
         elif (fl < fu):
             B1 = xu
             d=0.618*(B1-A1)
+            # print(d,B1,A1)
             xu = B1 - d
             fu = Fexample(xu)
             xl, xu, fl, fu = swaplarger(xl, xu, fl, fu)
-        J += 1
-        print(xl,xu)
+        J+=1
 
-GoldenSection(0,3,6)
+    print(xl, xu, fl, fu)
+
+GoldenSection(0,3,14)
